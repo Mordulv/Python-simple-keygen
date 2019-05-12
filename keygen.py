5ece2544c16f5538d8dc73557756fe4ba60ebbb4
Mordulv <50501108+Mordulv@users.noreply.github.com>
2019-05-12 16:08:00 +0200

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -3,44 +3,44 @@ from random import randint
 class Keygen():
 
     def __init__(self,bloques=8,cantidad = 6):
-        self.bloques = bloques
-        self.cantidad = cantidad
         
+        self.bloques, self.cantidad, self.suma_total = bloques, cantidad, int(90*cantidad*0.8)
+
     def letra_aleatoria(self):
+        
         return chr(randint(65,90))
 
     def bloque_letras_x(self):
-        texto = ''
-        suma_b1 = 0
-        cont_b1 = 0
-        suma_total = int(90*self.cantidad*0.8)
         
-        while suma_b1 != suma_total:
-            if suma_b1 != suma_total and cont_b1==self.cantidad:
+        texto, suma_b1, cont_b1 = '', 0, 0
+        
+        while suma_b1 != self.suma_total:
+            if suma_b1 != self.suma_total and cont_b1==self.cantidad:
                 suma_b1,cont_b1,texto = 0,0,''
             else:
                 letra = self.letra_aleatoria()
-                suma_b1 = suma_b1 + ord(letra)
-                texto = texto + letra
-                cont_b1 = cont_b1 + 1
+                suma_b1 +=  ord(letra)
+                texto +=  letra
+                cont_b1 +=  1
+                
         return texto
 
     def generar_key(self):
+        
         key = ''
         for i in range(self.bloques):
-            key = key + self.bloque_letras_x() + '-'
+            key +=  self.bloque_letras_x() + '-'
+            
         return key.strip('-')
 
     def validar_bloque(self,bloque):
-        suma_total = int(90*self.cantidad*0.8)
-        suma = 0
-        for i in bloque:
-            suma = suma + ord(i)
-        return suma == suma_total
+        
+        suma = sum([ord(i) for i in bloque])
+        
+        return suma == self.suma_total
 
     def validar_key(self,key):
-        suma = 0
-        for i in key.split('-'):
-            suma = suma + self.validar_bloque(i)
+        
+        suma = sum([self.validar_bloque(i) for i in key.split('-')])
 
         return suma == self.bloques
